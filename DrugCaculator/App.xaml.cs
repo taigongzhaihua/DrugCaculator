1e48a5cee334ae4bfc8e9fd363d2df955337cb21
Peter Jones <944541960@qq.com>
2024-11-07 01:44:30 +0800

    限制同时打开多个程序

--- DrugCaculator/App.xaml.cs
+++ DrugCaculator/App.xaml.cs
@@ -1,9 +1,30 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
 
 namespace DrugCaculator
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application;
+    public partial class App : Application
+    {
+        public static Mutex Mutex1 { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string mutexName = "DrugCalculator_SingleInstanceMutex";
+
+            Mutex1 = new Mutex(true, mutexName, out var createdNew);
+
+            if (!createdNew)
+            {
+                // 如果应用程序的实例已经存在，则退出新实例
+                MessageBox.Show("应用程序已经在运行中。");
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+    }
 }
